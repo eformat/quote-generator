e2b373002cec9e25343ef8f42345eebcd43b2350
Mike Hepburn <eformat@gmail.com>
2020-07-17 09:47:48 +1000

    🐇lists 🐇

--- src/main/java/org/acme/quotes/QuoteResource.java
+++ src/main/java/org/acme/quotes/QuoteResource.java
@@ -21,8 +21,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 @Path("/quotes")
